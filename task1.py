b9fc4cc4953b07a63ab2a39fad564882500c1593
suspensed <64693607+suspensed@users.noreply.github.com>
2021-01-14 17:39:27 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,6 @@
-#cd C:\Users\79632\desktop\python\GeekBrains
-
-#1 
 a = "1"
 b = 2
-try:
-    c = int(input("введите число "))
-except ValueError:
-    print('вы ввели строку.')
-
-
+c = int(input("введите число "))
 d = input("Введите строку ")
 
 print(a, b, c, d)
